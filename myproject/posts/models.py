3fca3cec46fae97bacd7d44449bda0f3e3b42eb3
matheeshaw25 <matheeshawijerathna@gmail.com>
2024-11-20 13:18:37 +0530

    Created models and migrations

--- myproject/posts/models.py
+++ myproject/posts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=75)
+    body = models.TextField()
+    slug = models.SlugField()
+    date = models.DateTimeField(auto_now_add=True) # auto_now_add creates a timestamp when user adds post
